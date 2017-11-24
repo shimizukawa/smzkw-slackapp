1699068c86c80edb3cf71babba77657af4486c74
Takayuki Shimizukawa <takayuki.shimizukawa@beproud.jp>
2017-11-24 17:07:37 +0900

    progress!

--- main.py
+++ main.py
@@ -33,11 +33,10 @@ def slash():
     print(bottle.request.body.read())  # DEBUG
     f = bottle.request.forms
     if f['token'] != TOKEN:
-        print(f['token'], TOKEN)
         raise bottle.HTTPError(400, 'Invalid Token')
 
     response_data = '''
-    {"text": "wanさんはshotですか?", "username": "まるこめ", "attachments": [ { "text": "1つ選んでね!", "fallback": "いまサーバー落ちてるわ", "callback_id": "wanshot", "color": "#3AA3E3", "attachment_type": "default", "actions": [ { "name": "aji", "text": "合わせ味噌", "type": "button", "value": "miso" }, { "name": "aji", "text": "料亭", "type": "button", "value": "ryoutei" }, { "name": "aji", "text": "極", "style": "danger", "type": "button", "value": "kiwami", "confirm": { "title": "まじで?", "text": "いっちゃう?", "ok_text": "まじで！", "dismiss_text": "やめとく" }}]}]}
+    {"response_type": "in_channel", "text": "wanさんはshotですか?", "username": "まるこめ", "attachments": [ { "text": "1つ選んでね!", "fallback": "いまサーバー落ちてるわ", "callback_id": "wanshot", "color": "#3AA3E3", "attachment_type": "default", "actions": [ { "name": "aji", "text": "合わせ味噌", "type": "button", "value": "miso" }, { "name": "aji", "text": "料亭", "type": "button", "value": "ryoutei" }, { "name": "aji", "text": "極", "style": "danger", "type": "button", "value": "kiwami", "confirm": { "title": "まじで?", "text": "いっちゃう?", "ok_text": "まじで！", "dismiss_text": "やめとく" }}]}]}
     '''
     bottle.response.content_type = 'application/json'
     return response_data
@@ -46,6 +45,7 @@ def slash():
 @app.route('/post', method='POST')
 def post():
     data = bottle.request.json
+    print(data)
     bottle.response.content_type = 'application/json'
     return data
 
