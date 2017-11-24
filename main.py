c4ed9055ceda3fe4e391a4ed7bcb66a18bb61218
Takayuki Shimizukawa <takayuki.shimizukawa@beproud.jp>
2017-11-24 17:32:59 +0900

    response message that depend button choosen

--- main.py
+++ main.py
@@ -35,21 +35,50 @@ def slash():
     if f['token'] != TOKEN:
         raise bottle.HTTPError(400, 'Invalid Token')
 
-    response_data = '''
-    {"response_type": "in_channel", "text": "wanさんはshotですか?", "username": "まるこめ", "attachments": [ { "text": "1つ選んでね!", "fallback": "いまサーバー落ちてるわ", "callback_id": "wanshot", "color": "#3AA3E3", "attachment_type": "default", "actions": [ { "name": "aji", "text": "合わせ味噌", "type": "button", "value": "miso" }, { "name": "aji", "text": "料亭", "type": "button", "value": "ryoutei" }, { "name": "aji", "text": "極", "style": "danger", "type": "button", "value": "kiwami", "confirm": { "title": "まじで?", "text": "いっちゃう?", "ok_text": "まじで！", "dismiss_text": "やめとく" }}]}]}
-    '''
+    response_data = {
+        "response_type": "in_channel",
+        "username": "まるこめ",
+        "text": "wanさんはshotですか?",
+        "attachments": [{
+            "text": "1つ選んでね!",
+            "fallback": "いまサーバー落ちてるわ",
+            "callback_id": "wanshot",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {"name": "aji", "text": "合わせ味噌", "type": "button", "value": "miso"},
+                {"name": "aji", "text": "料亭", "type": "button", "value": "ryoutei"},
+                {"name": "aji", "text": "極", "style": "danger", "type": "button", "value": "kiwami",
+                 "confirm": {"title": "まじで?", "text": "いっちゃう?",
+                             "ok_text": "まじで！", "dismiss_text": "やめとく"}},
+            ]
+        }]
+    }
     bottle.response.content_type = 'application/json'
-    return response_data
+    return json.dumps(response_data)
 
 
 @app.route('/post', method='POST')
 def post():
     payload = bottle.request.forms['payload']
-    print(payload)
     data = json.loads(payload)
-    print(data)
+    print(data)  # DEBUG
+    if data['token'] != TOKEN:
+        raise bottle.HTTPError(400, 'Invalid Token')
+
+    user_name = data['user']['name']
+    value = data['actions'][0]['value']
+    taste = ''
+
+    for a in data['original_message']['attachments'][0]['actions']:
+        if a['value'] == value:
+            taste = a['text']
+            break
+
+    text = f'{user_name}は `{taste}` を選びました'
+    response_data = {"response_type": "in_channel", "username": "まるこめ", "text": text}
     bottle.response.content_type = 'application/json'
-    return data
+    return json.dumps(response_data)
 
 
 if __name__ == '__main__':
