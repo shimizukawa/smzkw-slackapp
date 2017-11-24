4bffc47eee84f76b72e141765f971abf036e40dc
Takayuki Shimizukawa <takayuki.shimizukawa@beproud.jp>
2017-11-24 15:02:30 +0900

    add '/' for navigation

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ PORT = int(os.environ.get('PORT', '8080'))
 app = bottle.Bottle()
 
 
+@app.route('/')
+def ping():
+    bottle.response.content_type = 'text/html'
+    return """
+    <html><body>
+    <ul>
+    <li><a href="/ping">ping response in JSON for testing</a></li>
+    <li><a href="/post">POST button action from SlackApp</a></li>
+    </ul>
+    </body></html>
+    """
+
+
 @app.route('/ping')
 def ping():
     bottle.response.content_type = 'application/json'
